4ca7573c3e18bd97def754541464960d19096eb8
stringsplit <77242831+nbitzz@users.noreply.github.com>
2024-04-21 19:46:43 -0700

    Im going to kill myself

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './src/remark-plugins/readTime';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://split.pet/'
+    site: 'https://split.pet/',
+    markdown: {
+        remarkPlugins: [remarkReadingTime]
+    }
 });
---
+++ src/remark-plugins/readTime.ts
@@ -0,0 +1,11 @@
+// https://docs.astro.build/en/recipes/reading-time/
+import getReadingTime from 'reading-time';
+import { toString } from 'mdast-util-to-string';
+
+export function remarkReadingTime() {
+  return function (tree, { data }) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+    data.astro.frontmatter.readTime = readingTime;
+  };
+}
