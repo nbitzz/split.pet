f8913a0b663caa9f6cf1589e279f26f069d493d9
stringsplit <77242831+nbitzz@users.noreply.github.com>
2024-05-13 21:49:15 -0700

    mdx support, ish

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,5 +1,5 @@
 import rss, {pagesGlobToRssItems} from '@astrojs/rss';
-import type { MarkdownInstance } from 'astro';
+import type { MarkdownInstance, MDXInstance } from 'astro';
 import type { Props as PostProps } from "../layouts/Post.astro"
 
 export async function GET(context) {
@@ -8,15 +8,15 @@ export async function GET(context) {
     description: 'blog hosted on split.pet by split!',
     site: context.site,
     items: 
-      (await Promise.all(Object.entries(import.meta.glob("./blog/*.md"))
+      (await Promise.all(Object.entries(import.meta.glob<MarkdownInstance<PostProps["frontmatter"]>|MDXInstance<PostProps["frontmatter"]>>("./blog/*.{md,mdx}"))
         .map(([_, get]) => get())))
-        .filter((e: MarkdownInstance<PostProps["frontmatter"]>) => e.frontmatter.publicationDate && !e.frontmatter.unlisted)
-        .map((e: MarkdownInstance<PostProps["frontmatter"]>) => {
+        .filter(e => e.frontmatter.publicationDate && !e.frontmatter.unlisted)
+        .map(e => {
           return {
             title: e.frontmatter.title,
             description: e.frontmatter.description,
             pubDate: new Date(e.frontmatter.publicationDate),
-            content: e.compiledContent(),
+            content: "compiledContent" in e ? e.compiledContent() : `This content can't be rendered here. See the full post at <a href="${e.url}">${e.url}</a>.`,
             link: e.url
           }
         })
