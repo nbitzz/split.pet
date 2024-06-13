5ad0c290d3f31acad71b861c9511b21e60360244
stringsplit <77242831+nbitzz@users.noreply.github.com>
2024-06-12 19:39:34 -0700

    accessibility

--- src/util/ohgodpleasehelpme.ts
+++ src/util/ohgodpleasehelpme.ts
@@ -1,4 +1,7 @@
-// this unfortunately seems to be the intended way of doing this shit
+// if you're reading what's in this file i'm Sorry
+
+// using parseSync cause I don't gaf
+import { parseSync as parseSvg, stringify as buildSvg } from "svgson"
 
 let metadata =
         await import.meta.glob<{default: ImageMetadata}>("../images/**/*")
@@ -7,4 +10,24 @@ export default function dynImportImg(path: string) {
     let img = metadata[`../images/${path}`]
     if (!img) throw `${path} not found`
     return img()
+}
+
+export function injectTitleIntoSvg(input: string, title: string) {
+    let svg = parseSvg(input)
+    svg.children.splice(0,0,{
+        name: "title",
+        type: "element",
+        value: "",
+        attributes: {},
+        children: [
+            {
+                name: "",
+                type: "text",
+                value: title,
+                attributes: {},
+                children: []
+            }
+        ],
+    })
+    return buildSvg(svg)
 }
