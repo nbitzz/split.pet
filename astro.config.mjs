2cf7965954a2f2cfd89ebb93cb4d9418429cbb3e
split <split@split.pet>
2024-08-29 20:34:12 -0700

    worst code ive ever written

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './src/remark-plugins/readTime';
 import { remarkCustomEmoji } from './src/remark-plugins/customEmoji';
 import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://split.pet/',
