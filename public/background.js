e5bf6068c8ecd4453979ee500b7c693b41296c63
split <split@split.pet>
2024-11-12 21:48:04 -0800

    fix: :bug: cache current document, fix back button

--- public/background.js
+++ public/background.js
@@ -1,15 +1,19 @@
-onmessage = async ({ data: url }) => {
-    let { abort, signal } = new AbortController(),
-        r = await fetch(url, { signal })
+onmessage = async ({ data: { url = undefined, dom = undefined } }) => {
+    if (!dom) {
+        let { abort, signal } = new AbortController(),
+            r = await fetch(url, { signal })
 
-    if (!r.headers.get("content-type")?.includes("text/html")) {
-        abort()
-        return postMessage({ url, d: false })
+        if (!r.headers.get("content-type")?.includes("text/html")) {
+            abort()
+            return postMessage({ url, d: false })
+        }
+
+        dom = await r.text()
     }
 
     postMessage({
         url,
-        d: (await r.text()).match(
+        d: dom.match(
             /<head>([\S\s]*?)<\/head>[\S\s]*<div id="all".*?>([\S\s]*)<\/div>.*<\/div>/
         )?.slice(1),
     })
