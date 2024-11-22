abc0b3d0d9a7580b5a1b10c4983205ee3cbbfc34
split <split@split.pet>
2024-11-22 09:39:15 -0800

    feat: :sparkles: ASS feed

---
+++ src/pages/feed.ass.ts
@@ -0,0 +1,23 @@
+import rss, { pagesGlobToRssItems } from "@astrojs/rss"
+import type { MarkdownInstance, MDXInstance } from "astro"
+import type { Props as PostProps } from "../layouts/Post.astro"
+import { getCollection } from "astro:content"
+import astroConfig from "../../astro.config.mjs"
+
+export async function GET(context) {
+    const posts = await getCollection("posts")
+    return new Response(
+        "# Actually Simple Syndication - https://tilde.town/~dzwdz/ass/\n" +
+            posts
+                .filter(e => e.data.pubDate && !e.data.unlisted)
+                .map(
+                    e =>
+                        `${
+                            new Date(e.data.pubDate!)
+                                .toISOString()
+                                .split("T")[0]
+                        }\t${astroConfig.site}/${e.slug}\t${e.data.title}`
+                )
+                .join("\n")
+    )
+}
